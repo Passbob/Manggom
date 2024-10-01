3a333e676ce80c664132070cd24c27ca159f9168
Passbob <gudjtr097@gmail.com>
2024-10-02 00:03:41 +0900

    Update InputUserInfo.java

--- src/main/java/com/manggom/play/InputUserInfo.java
+++ src/main/java/com/manggom/play/InputUserInfo.java
@@ -5,9 +5,6 @@ import com.manggom.model.dto.ExitDTO;
 import com.manggom.model.dto.UserDTO;
 
 import java.sql.Connection;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 import static com.manggom.common.JDBCTemplate.getConnection;
@@ -22,12 +19,12 @@ public class InputUserInfo {
 
         for (int i = 0; true; i++) {
             System.out.println("안녕하세요. 망곰이 굿즈 구매사이트입니다.");
-            System.out.print("구매를 원하신다면 1, 재구매를 원하신다면 2, 나가기를 원하신다면 3을 입력해주세요. : ");
+            System.out.print("구매를 원하신다면 1, 재구매를 하신다면 2, 나가기를 원하신다면 3을 입력해주세요. : ");
             int choice = sc.nextInt();
             if (choice == 1) {
                 inputUserInfo();
             } else if (choice == 2) {
-
+                rePurchaseUser();
 
             } else if (choice == 3) {
                 ExitDTO exit = new ExitDTO();
@@ -81,13 +78,15 @@ public class InputUserInfo {
         }
     }
 
-    public void RePurchaseUser() {
+    public void rePurchaseUser() {
 
-
+    //  기존고객 조회
         Connection con = getConnection();
         UserDAO resistDAO = new UserDAO();
+        UserDTO user = new UserDTO();
 
-        for (int i = 0; true; i++) {
+
+
             System.out.println();
             System.out.print("이름을 입력해주세요. : ");
             sc.nextLine();
@@ -96,20 +95,22 @@ public class InputUserInfo {
             String userPhone = sc.nextLine();
 
             if (userName != "" && userPhone != "") {
-                Map<String , String> comparison = new HashMap<>();
-                comparison.put(userName, userPhone);
-
-
-
+                user = resistDAO.comparisonUserInfo(con, userName, userPhone);
+                if(user != null) {
+                    resistDAO.inputUserInfo(con, user);
+                    System.out.println("조회에 성공했습니다.");
+                }else{
+                    System.out.println("정보가 없습니다.");
+                }
 
             } else {
                 System.out.println();
-                System.out.println("정보를 다시 입력해주세요.");
+                System.out.println("정보를 잘못입력했습니다.");
                 System.out.println();
             }
 
 
-        }
+
 
     }
 }
